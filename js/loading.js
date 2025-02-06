10f985ac2f98eee340f1da432517c4ed284984f6
Jayzk707 <Jayzk707@users.noreply.github.com>
2025-02-06 05:36:50 +0000

    auto deploy Jayzk707/hugo-min@10c2ef638603d88d8defbbc26504ded6555300a4

--- js/loading.js
+++ js/loading.js
@@ -10,28 +10,7 @@ document.addEventListener("DOMContentLoaded", function () {
         // 延迟500ms后隐藏整个loading屏幕
         setTimeout(() => {
             document.querySelector(".loading-screen").classList.add("hide");
-        }, 300);  // 延迟500ms后隐藏，确保动画平滑过渡
+        }, 500);  // 延迟500ms后隐藏，确保动画平滑过渡
     });
 });
 
-// 页面加载时的进度条逻辑
-document.addEventListener("DOMContentLoaded", function () {
-    // 配置进度条
-    topbar.config({
-        barThickness: 5, // 进度条的高度
-        barColors: { 
-            "0": "rgba(0, 149, 234, 0.8)",  // 进度条开始时的颜色
-            "1.0": "rgba(0, 255, 0, 0.8)" // 进度条结束时的颜色
-        },
-        shadowBlur: 10, // 进度条的阴影模糊程度
-        shadowColor: "rgba(0, 0, 0, 0.5)" // 进度条的阴影颜色
-    });
-
-    // 页面加载时显示进度条
-    requestAnimationFrame(() => topbar.show());
-
-    // 页面加载完成后隐藏进度条
-    window.addEventListener("load", function () {
-        setTimeout(() => topbar.hide(), 300); // 延迟500ms隐藏
-    });
-});
